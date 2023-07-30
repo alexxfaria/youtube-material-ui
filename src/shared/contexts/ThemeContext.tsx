4dbd25af7f41e284cb680a96bce39b7f13a1d773
Alex <alexxfs@live.com>
2023-07-30 19:01:44 -0300

    feat: add toggle theme

---
+++ src/shared/contexts/ThemeContext.tsx
@@ -0,0 +1,40 @@
+import { createContext, useState, useCallback, useMemo,useContext } from "react";
+import { ThemeProvider, Box } from "@mui/material";
+import { LightTheme, DarkTheme } from "./../themes";
+
+interface IThemeContextData {
+  themeName: "light" | "dark";
+  toggleTheme: () => void;
+}
+interface IAppThemeProviderProps {
+  children: React.ReactNode;
+}
+
+const ThemeContext = createContext({} as IThemeContextData);
+
+export const AppThemeProvider: React.FC<IAppThemeProviderProps> = ({
+  children,
+}) => {
+  const [themeName, setThemeName] = useState<"light" | "dark">("light");
+  const toggleTheme = useCallback(() => {
+    setThemeName((oldThemeName) =>
+      oldThemeName === "light" ? "dark" : "light"
+    );
+  }, []);
+  const theme = useMemo(() => {
+    if (themeName === "light") return LightTheme;
+    return DarkTheme;
+  }, [themeName]);
+
+  return (
+    <ThemeContext.Provider value={{ themeName, toggleTheme }}>
+      <ThemeProvider theme={theme}>
+        <Box width={"100vw"} height={"100vh"} bgcolor={theme.palette.background.default}>{children}</Box>
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
+
+export const useAppThemeContext = () => {
+   return useContext(ThemeContext);
+};
